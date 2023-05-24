df4e4a892541da377917a2ebabb6645caa74fb16
sahilhedau49 <hedausahil49@gmail.com>
2023-05-24 08:50:17 +0530

    Setup Done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="m-20 font-extrabold text-4xl text-teal-500">
+        Hello World
+      </h1>
+    </div>
+  );
+}
+
+export default App;
